b4499086f5ade3a3089fc7eb2b05477154785970
Wenxin Wang <wenxin.wang94@gmail.com>
2017-06-10 10:19:50 +0800

    prepare eval

--- eval.sh
+++ eval.sh
@@ -3,6 +3,6 @@ MODEL_DIR=$DATA_DIR/model
 RECORDS_DIR=$DATA_DIR/records
 
 python evaluate.py \
-  --input_file_pattern="${RECORDS_DIR}/trn-?????-of-00016" \
-  --train_dir="${MODEL_DIR}/train" \
-  --number_of_steps=1000000
+  --input_file_pattern="${RECORDS_DIR}/val-?????-of-00002" \
+  --checkpoint_dir="${MODEL_DIR}/train" \
+  --eval_dir="${MODEL_DIR}/eval"
